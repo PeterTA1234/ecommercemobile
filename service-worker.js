e1944dee118e005e57fd0148692cbf6cc7d7cff4
PeterTA1234 <Peterggc13@gmail.com>
2026-04-01 06:04:45 +0500

    Modify service worker to cache e-commerce files
    Updated the service worker to cache specific files for the e-commerce mobile site.

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,18 @@
 self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('store-cache').then(function(cache) {
+      return cache.addAll([
+        '/ecommercemobile/index.html',
+        '/ecommercemobile/style.css'
+      ]);
+    })
+  );
+});
 
-event.waitUntil(
-
-caches.open('store-cache').then(function(cache) {
-
-return cache.addAll([
-'/',
-'/index.html',
-'/style.css'
-]);
-
-})
-
-);
-
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
